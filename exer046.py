babfac83af5caf2edb8f36b4583f047daa3a7209
Gabriel Peres Magni <gpmagni@gmail.com>
2018-07-12 19:07:59 -0300

    Update exer046.py

--- exer046.py
+++ exer046.py
@@ -1,7 +1,7 @@
 from time import sleep
-x = 0
-for esperar in range(0, 10):
+x = 11
+for esperar in range(0, 11):
     sleep(1)
-    x += 1
+    x -= 1
     print('\033[30m', str(x) + '!')
 print('\033[1;31mBOOOM')
